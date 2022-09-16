735eedd62d719e19559b8e914394215aa9ab039a
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-16 08:51:43 +0200

    Update the PickerView list by show list of breeds there - no images yet

--- Dogs/Controller/ViewController.swift
+++ Dogs/Controller/ViewController.swift
@@ -6,11 +6,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var randomImage: UIImageView!
     
-    let breeds: [String] = ["hau","Mio","3ao"]
+    var breeds: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.dataSource = self
         pickerView.delegate = self
+        DogApi.requestListOfImage(completionHandler: handleListofBreeds(breeds:error:))
     }  
 }
 
@@ -30,6 +31,14 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         DogApi.requestRandomImage(breed: breeds[row] ,completionHandler: handleRandomImageResponse(imageData:error:))
     }
+    func handleListofBreeds(breeds: [String], error: Error?) {
+        // update the breeds list here
+        self.breeds = breeds
+        DispatchQueue.main.async {
+            self.pickerView.reloadAllComponents()
+        }
+    }
+    
     func handleRandomImageResponse(imageData: DogImage?, error: Error?) {
         guard let imageURL = URL(string: imageData?.message ?? "" ) else { return }
         DogApi.requestImageFile(url: imageURL, completionHandler: self.handleImageFileResponse(image:error:))
